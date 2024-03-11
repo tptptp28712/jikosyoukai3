2d3a81e232dd5031a765fc1c357ff51a6b8398ae
tptptp28712 <159972348+tptptp28712@users.noreply.github.com>
2024-03-12 01:35:10 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ package jk1;
 import java.util.*;
 class Main{
 public static void main(String[] args){
-Person person1 = new Person("鈴木", "太郎", 20, 1.7, 60);
+jk1.Person person1 = new jk1.Person("鈴木", "太郎", 20, 1.7, 60);
 person1.print();
 
-Person person2 = new Person("山田", "花子", 22, 1.5, 40);
+jk1.Person person2 = new jk1.Person("山田", "花子", 22, 1.5, 40);
 person2.print();
 
 Person.printCount();
